7fe65d898b9b9bca1bc90ebbc5ba0bf7eee81c7d
A. Vatsaev <azero79@gmail.com>
2014-07-14 13:59:25 +0200

    Initial Commit

---
+++ Swift_Car/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Car
+//
+//  Created by Vatsaev Aslan on 14/07/2014.
+//  Copyright (c) 2014 Aslan Vatsaev. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
